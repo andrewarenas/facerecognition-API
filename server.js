74e6d857b786621db77316031b8e13bd3d63b27e
Andres Arenas <andres.ar3nas@gmail.com>
2019-07-24 10:45:36 -0500

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
+		host: 'postgresql-regular-39625',
 		user: 'postgres',
 		password: 'andresarenas123',
 		database: 'smart-brain'
